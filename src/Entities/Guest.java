2e937c9480e039b15cbbb7226a25f949270a8580
Dreeeamed <lookuptoyourdreams@gmail.com>
2026-01-25 18:46:53 +0500

    Add Entity - Guest

---
+++ src/Entities/Guest.java
@@ -0,0 +1,43 @@
+package Entities;
+public class Guest {
+    private int id;
+    private String fullName;
+    private String email;
+
+    //DEFAULT CONSTRUCTOR
+    public Guest() {
+        this.id = 0;
+    } //Etot Part useless tbh
+
+    //PARAMETER CONSTRUCTOR
+    public Guest(String fullName, String email) {
+        this();
+        setName(fullName);
+        setEmail(email);
+    }
+
+    //ENCAPSULATION
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return fullName; }
+    public void setName(String fullName) {
+        if(fullName == null || fullName.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) {
+        if(email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+        this.email = email; }
+
+
+    //OTHER THINGS
+    @Override
+    public String toString() {
+        return "id=" + id + ", name=" + fullName;
+    }
+}
